a41981994fcd897edd9dafe9cc771bf78c974ebe
yudii2 <qleen513@gmail.com>
2021-09-15 20:24:27 +0900

    [김유진] conflict test2

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -2,8 +2,9 @@ package test.http;
 
 public class GitTest {
 	
-	private static final String DESC = "[github]local GIT Test용 클래스 입니다.";
-	private static final String DEV = "dev브랜치 테스트 시작[local]";
+	private static final String DESC = "local GIT Test용 클래스 입니다.";
+	private static final String DEV = "dev브랜치 테스트 시작";
 	private static final String DEV_FIN = "dev브랜치 테스트 종료";
-
+	private static final String CONFLICT = "Conflict 테스트";
+	private static final String MERGETOOL = "merge tool 사용";
 }
